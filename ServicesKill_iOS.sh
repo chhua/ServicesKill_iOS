b3894a66977d60284e65cb0364ca2018d6c95a15
chhua <tcicr4j@gmail.com>
2025-10-20 04:09:09 +0800

    Update ServicesKill_iOS.sh

--- ServicesKill_iOS.sh
+++ ServicesKill_iOS.sh
@@ -1,42 +1,78 @@
 #!/bin/sh
-cp -r /System/Library/LaunchDaemons/ /var/root #bak path
 
-echo "Type 'a' Make your iDevice better."
-echo "Type 'b' Restore backup."
-echo "Type 'c' Delete this script."
+###Only work under rootful devices###
 
-read -p "" choose
+BACKUP_PATH="/var/root/LaunchDaemonsBackup"
+DAEMON_PATH="/System/Library/LaunchDaemons"
+DAEMONS_TO_REMOVE="
+com.apple.DumpPanic.plist
+com.apple.DumpBasebandCrash.plist
+com.apple.ReportCrash*.plist
+com.apple.CrashHouseKeeping.plist
+com.apple.powerlog.plist
+com.apple.chud.chum.plist
+com.apple.chud.pilotfish.plist
+com.apple.mobile.profile_janitor.plist
+com.apple.aslmanager.plist
+com.apple.syslogd.plist
+com.apple.mobile.obliteration.plist
+com.apple.printd.plist
+com.apple.marcoagent.plist
+com.apple.mobile.installd.mount_helper.plist
+com.apple.mobile.storage_mounter.plist
+com.apple.AddressBook.plist
+com.apple.AdminLite.plist
+com.apple.appsupport.cplogd.plist
+com.apple.crash_mover.plist
+com.apple.mobile.softwareupdated.plist
+com.apple.OTACrashCopier.plist
+com.apple.OTATaskingAgent.plist
+com.apple.softwareupdateservicesd.plist
+com.apple.Springboard.SetupHelper.plist
+com.apple.certui.relay.plist
+com.apple.sharktrace.plist
+com.apple.daily.plist
+com.apple.wapic.plist
+com.apple.midiserver-ios.plist
+"
 
-if [ $choose = "a" ]; then
-	echo "Deleting Services Daemon..."
-	cd /System/Library/LaunchDaemons/
-	rm -rf com.apple.DumpPanic.plist com.apple.DumpBasebandCrash.plist com.apple.ReportCrash*.plist 
-	sleep 1
-	rm -rf com.apple.CrashHouseKeeping.plist com.apple.powerlog.plist com.apple.chud.chum.plist com.apple.chud.pilotfish.plist 
-	sleep 1
-	rm -rf com.apple.mobile.profile_janitor.plist com.apple.aslmanager.plist com.apple.syslogd.plist com.apple.mobile.obliteration.plist 
-	sleep 1
-	rm -rf com.apple.printd.plist com.apple.marcoagent.plist com.apple.mobile.installd.mount_helper.plist 
-	sleep 1
-	rm -rf com.apple.mobile.storage_mounter.plist com.apple.AddressBook.plist com.apple.AdminLite.plist
-	sleep 1 
-	rm -rf com.apple.appsupport.cplogd.plist com.apple.crash_mover.plist com.apple.mobile.softwareupdated.plist 
-	sleep 1
-	rm -rf com.apple.OTACrashCopier.plist com.apple.OTATaskingAgent.plist com.apple.softwareupdateservicesd.plist 
-	sleep 1
-	rm -rf com.apple.Springboard.SetupHelper.plist com.apple.certui.relay.plist com.apple.sharktrace.plist com.apple.daily.plist 
-	sleep 1
-	rm -rf com.apple.wapic.plist com.apple.midiserver-ios.plist
-	sleep 1
-	echo "completed!!"
-	cd /var/root/
-elif [ $choose = "b" ]; then
-	cp -rf LaunchDaemons/ /System/Library/
-	echo "Restore done will restart in 10sec..." 
-	sleep 10
-	reboot
-elif [ $choose = "c" ]; then
-	echo "Done" && exec rm $0 
-else 
-	exit 0
-fi
+echo "Choose an action:"
+echo "[a] Optimize system (remove selected daemons)"
+echo "[b] Restore backup"
+echo "[c] Delete this script"
+
+read -r -p "Enter your choice (a/b/c): " choose
+
+case "$choose" in
+  a)
+    echo "Creating backup..."
+    mkdir -p "$BACKUP_PATH"
+    cp -r "$DAEMON_PATH"/* "$BACKUP_PATH"/
+    echo "Backup complete. Starting daemon removal..."
+
+    for daemon in $DAEMONS_TO_REMOVE; do
+      rm -f "$DAEMON_PATH"/$daemon 2>/dev/null && echo "Removed: $daemon"
+      sleep 0.5
+    done
+
+    echo "Optimization complete!"
+    ;;
+
+  b)
+    echo "Restoring backup..."
+    cp -r "$BACKUP_PATH"/* "$DAEMON_PATH"/
+    echo "Restore complete. System will reboot in 10 seconds..."
+    sleep 10
+    reboot
+    ;;
+
+  c)
+    echo "Deleting script..."
+    rm -- "$0"
+    ;;
+
+  *)
+    echo "Invalid option. Exiting."
+    exit 1
+    ;;
+esac
